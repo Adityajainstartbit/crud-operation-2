5a603b4d47857f56ac5119456c742b242944631e
adityaru7 <adityajainstartbitsolutions.com>
2023-02-24 18:32:33 +0530

    isup to date

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article <ApplicationRecord
 	
 	validates:title,presence:true , length:{minium:6,maximum:25}
-	validates:description,presence:true,length:{minium:25,maximum:100}
+	validates:description,presence:true,length:{minium:25,maximum:2000}
 
 end
